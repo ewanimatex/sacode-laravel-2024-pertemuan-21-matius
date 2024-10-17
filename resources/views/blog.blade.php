6deed056f039267337fdc8decca22ae9e2a4012a
Matius ewani <ewanimatex@gmail.com>
2024-10-17 04:28:22 -0700

    Belajar laravel11

---
+++ resources/views/blog.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>halaman blog</title>
+</head>
+<body>
+    <h1>Blog</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias ipsa voluptatem architecto pariatur eos aliquam illum odio nesciunt quaerat et expedita qui reprehenderit blanditiis enim maiores placeat, tempora omnis corporis necessitatibus nobis facilis? Facere neque quis explicabo quae, enim earum delectus praesentium soluta repellendus amet repellat inventore saepe dolorem iure.</p>
+</body>
+</html>
